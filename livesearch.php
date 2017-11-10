90fcc875af2d99e3065e9d9f7426cf37faf2ef9c
Gautham Kumar <heygautham@gmail.com>
2017-11-10 22:02:38 +0530

    password updates

--- livesearch.php
+++ livesearch.php
@@ -1,10 +1,6 @@
 <?php
 $username=$_GET['q'];
-$username_server="root";
-$database="task1DB";
-$password_server="";
-$server="localhost";
-$conn=new mysqli($server, $username_server, $password_server, $database);
+require("config.php");
 $sql=" SELECT * FROM Users WHERE username = "."'".$username."'";
 $result=$conn->query($sql);
 if($result->num_rows==0)
